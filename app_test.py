afebf317193a7fc09faf321a2b3fa2481a4819ce
VjosaKuqi55 <85831974+VjosaKuqi55@users.noreply.github.com>
2022-12-23 17:12:50 +0100

    Create app_test.py

---
+++ app_test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Omg, here I am')
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+st.markdown('[Google](https://www.youtube.com/watch?v=ogG2fPLY544)')
+
